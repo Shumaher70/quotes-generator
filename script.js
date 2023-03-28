3bb52191f97b78502ce51e001f0213153f15110a
oleg <olegvraby@gmail.com>
2023-03-28 13:24:06 +0200

    fix files index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-const commentEl = document.querySelector('.qote_quote_box');
-const autorEl = document.querySelector('.name_quote_box');
-const btnEl = document.querySelector('.btn');
-let urls = [
-  'https://jsonplaceholder.typicode.com/comments',
-  'https://randomuser.me/api/',
-];
-createElements();
-function createElements() {
-  Promise.all(urls.map((url) => fetch(url)))
-    .then((response) => Promise.all(response.map((res) => res.json())))
-    .then((arrPromise) => {
-      const textCorrently = (str) =>
-        (b = str.charAt(0).toUpperCase() + str.slice(1));
+const content = document.querySelector('.content_quote');
+const author = document.querySelector('.author_quote');
+const btn = document.querySelector('.btn');
 
-      autorEl.textContent = `${arrPromise[1].results[0].name.last} ${arrPromise[1].results[0].name.first}`;
-      commentEl.textContent = `${textCorrently(
-        arrPromise[0][randomNum()].body
-      )}`;
+let url = 'https://api.quotable.io/random';
+
+const createQuote = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((quotes) => {
+      content.textContent = quotes.content;
+      author.textContent = quotes.author;
     });
-}
+};
 
-btnEl.addEventListener('click', createElements);
+btn.addEventListener('click', createQuote);
 
-function randomNum() {
-  const num = Math.floor(Math.random() * 500);
-  return num;
-}
+createQuote();
